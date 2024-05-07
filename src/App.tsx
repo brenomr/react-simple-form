cee25baf8a4121f5ec88207c496c592dc94b585e
Breno <brenocartoon@hotmail.com>
2024-05-07 16:58:26 -0300

    feat: add technologies field and show form object on screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { useForm } from "react-hook-form";
+import { useForm, useFieldArray } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { z } from "zod";
 
@@ -27,21 +27,44 @@ const createUserFormSchema = z.object({
       "Email deve ser do domínio gmail.com"
     ),
   password: z.string().min(6, "Senha deve ter no mínimo 6 caracteres"),
+  technologies: z
+    .array(
+      z.object({
+        title: z.string().min(1, "Título é obrigatório"),
+        knowledge: z.coerce
+          .number()
+          .min(1, "Conhecimento é obrigatório")
+          .max(5, "Conhecimento deve ser entre 1 e 5"),
+      })
+    )
+    .min(1, "Deve ter no mínimo uma tecnologia"),
 });
 
 type createUserFormData = z.infer<typeof createUserFormSchema>;
 
 function App() {
+  const [output, setOutput] = useState("");
+
   const {
     register,
     handleSubmit,
     formState: { errors },
+    control,
   } = useForm<createUserFormData>({
     resolver: zodResolver(createUserFormSchema),
   });
 
+  const { fields, append, remove } = useFieldArray({
+    control,
+    name: "technologies",
+  });
+
   const createUser = (data: any) => {
-    console.log(data);
+    setOutput(JSON.stringify(data));
+  };
+
+  const addNewTechnology = () => {
+    append({ title: "", knowledge: 0 });
   };
 
   return (
@@ -57,7 +80,9 @@ function App() {
             className="border border-zinc-500 shadow-sm rounded h-10 px-3 bg-zinc-800"
             {...register("name")}
           />
-          {errors.name && <span>{errors.name.message}</span>}
+          {errors.name && (
+            <span className="text-red-600">{errors.name.message}</span>
+          )}
         </div>
         <div className="flex flex-col gap-1">
           <label htmlFor="">Email</label>
@@ -66,7 +91,9 @@ function App() {
             className="border border-zinc-500 shadow-sm rounded h-10 px-3 bg-zinc-800"
             {...register("email")}
           />
-          {errors.email && <span>{errors.email.message}</span>}
+          {errors.email && (
+            <span className="text-red-600">{errors.email.message}</span>
+          )}
         </div>
 
         <div className="flex flex-col gap-1">
@@ -76,7 +103,51 @@ function App() {
             className="border border-zinc-500 shadow-sm rounded h-10 px-3 bg-zinc-800"
             {...register("password")}
           />
-          {errors.password && <span>{errors.password.message}</span>}
+          {errors.password && (
+            <span className="text-red-600">{errors.password.message}</span>
+          )}
+        </div>
+        <div className="flex flex-col gap-1">
+          <label htmlFor="" className="flex items-center justify-between">
+            Tecnologias
+            <button
+              className="text-emerald-600 text-sm"
+              onClick={addNewTechnology}
+            >
+              Adicionar
+            </button>
+          </label>
+          {fields.map((field, index) => (
+            <div key={field.id}>
+              <div className="grid grid-cols-3 gap-4">
+                <input
+                  type="text"
+                  className="col-span-2 border border-zinc-500 shadow-sm rounded h-10 px-3 bg-zinc-800"
+                  {...register(`technologies.${index}.title` as const)}
+                />
+                <input
+                  type="number"
+                  className="col-span-1 border border-zinc-500 shadow-sm rounded h-10 px-3 bg-zinc-800"
+                  {...register(`technologies.${index}.knowledge` as const)}
+                />
+              </div>
+              <div className="flex flex-col">
+                {errors.technologies?.[index]?.title && (
+                  <span className="text-red-600">
+                    {errors.technologies[index]?.title?.message}
+                  </span>
+                )}
+                {errors.technologies?.[index]?.knowledge && (
+                  <span className="text-red-600">
+                    {errors.technologies[index]?.knowledge?.message}
+                  </span>
+                )}
+              </div>
+            </div>
+          ))}
+          {errors.technologies && (
+            <span className="text-red-600">{errors.technologies.message}</span>
+          )}
         </div>
 
         <button
@@ -86,6 +157,7 @@ function App() {
           Salvar
         </button>
       </form>
+      <pre>{output}</pre>
     </main>
   );
 }
